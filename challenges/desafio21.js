b37b23b7d1394d0b8f206728818fcdf3917e1afd
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-12-16 16:59:23 -0300

    feat: add challenge 21

---
+++ challenges/desafio21.js
@@ -0,0 +1,6 @@
+db.produtos.updateOne(
+  { nome: { $in: ['Cheddar McMelt'] } },
+  { $pull: { ingredientes: 'pão escuro com gergelim' } }
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
